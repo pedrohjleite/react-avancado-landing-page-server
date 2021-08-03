436b16e709d05ebeec8b4e35db500b5a42de4fca
Pedro Henrique Jansen Leite <pedrohjleite@hotmail.com>
2021-08-03 20:54:48 -0300

    First Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9122fa71c12254970f35f2f544407309'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8766ce34-24da-4aba-b998-84a5e912af24'
+};
